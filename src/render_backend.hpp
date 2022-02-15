b9ec558a23bb96efaa298bc387f0b726a661312a
Max Penkov <hello@maxpenkov.com>
2022-02-15 22:53:23 +0200

    Update source file 'render_backend.hpp'

--- src/render_backend.hpp
+++ src/render_backend.hpp
@@ -1,13 +1,10 @@
 /**
  * @file render_backend.hpp
- * @date 11/02/2022
  * @date 14/02/2022
  * 
  * @brief 
  */
 
-#ifndef GRAPHICS_HANDLER_HPP
-#define GRAPHICS_HANDLER_HPP
 #ifndef RENDER_BACKEND_HPP
 #define RENDER_BACKEND_HPP
 
@@ -16,39 +13,79 @@
 #include <d2d1.h>
 #include <d2d1_1.h>
 
-#include "utility.hpp"
-
-class graphics_handler
-{
-public:
+namespace render_backend {
 	
-	graphics_handler() = default;
+	/**
+	 * @brief
+	 */
+	extern HWND	g_window_handle;
 
-	static auto create_window(const wchar_t* title_name) -> HWND;
-	
-	static auto create_native_window(HWND& window_handle, WNDCLASSEX& window_class,WNDPROC wndproc, const wchar_t* class_name, const wchar_t* window_name, int posX, int posY, int width, int height, DWORD extended_window_style = 0, DWORD window_style = 0) -> BOOL;
+	/**
+	 * @brief
+	 */
+	extern IDXGISwapChain* g_swapchain;
 
-	static auto get_window_size(HWND handle, int& height, int& width) -> BOOL;
-	
-	static auto create_d3d11_device(HWND window_handle, IDXGISwapChain*& swapchain, ID3D11Device*& device, ID3D11DeviceContext*& device_context) -> BOOL;
+	/**
+	 * @brief
+	 */
+	extern ID3D11Device* g_d3d11_device;
 
-	static auto create_d3d11_render_target(IDXGISwapChain* swapchain, ID3D11Device* device, ID3D11RenderTargetView*& render_target) -> BOOL;
+	/**
+	 * @brief
+	 */
+	extern ID3D11DeviceContext* g_d3d11_device_context;
 
-	static auto create_d2d1_device_context(IDXGISwapChain* swapchain, ID3D11Device* device, ID2D1DeviceContext*& device_context) -> BOOL;
+	/**
+	 * @brief
+	 */
+	extern ID3D11RenderTargetView* g_d3d11_render_target;
 
-	static auto create_d2d11_render_target(IDXGISwapChain* swapchain, ID2D1RenderTarget*& d2d1_render_target) -> BOOL;
+	/**
+	 * @brief
+	 */
+	static BOOL d3d11_create_device(const HWND window_handle, IDXGISwapChain*& swapchain, ID3D11Device*& device, ID3D11DeviceContext*& device_context);
 
-	static auto release_dxgi_swapchain(IDXGISwapChain* swapchain) -> void;
+	/**
+	 * @brief
+	 */
+	static void d3d11_release_device(ID3D11Device* device);
 
-	static auto release_d3d11_device(ID3D11Device* device) -> void;
+	/**
+	 * @brief
+	 */
+	static BOOL d3d11_create_render_target(IDXGISwapChain* swapchain, ID3D11Device* device, ID3D11RenderTargetView*& render_target);
 
-	static auto release_d3d11_device_context(ID3D11DeviceContext* device_context) -> void;
+	/**
+	 * @brief
+	 */
+	static void d3d11_release_render_target(ID3D11RenderTargetView* render_target);
 
-	static auto release_d3d11_render_target(ID3D11RenderTargetView* render_target) -> void;
+	/**
+	 * @brief
+	 */
+	static void d3d11_release_device_context(ID3D11DeviceContext* device_context);
 
-	static auto release_d2d1_device_context(ID2D1DeviceContext* device_context) -> void;
+	/**
+	 * @brief
+	 */
+	static void dxgi_release_swapchain(IDXGISwapChain* swapchain);
 
-	static auto release_d2d11_render_target(ID2D1RenderTarget* d2d1_render_target) -> void;
+	/**
+	 * @brief
+	 */
+	BOOL initialize();
+
+	/**
+	 * @brief
+	 */
+	void terminate();
+}
+
+namespace window {
+
+	BOOL create_window(HWND& window_handle, WNDCLASSEX& window_class, WNDPROC wndproc, const char* class_name, const char* window_name, int posX, int posY, int width, int height, DWORD extended_window_style = 0, DWORD window_style = 0);
+
+	BOOL get_window_size(HWND handle, int& height, int& width);
 };
 
 #endif
