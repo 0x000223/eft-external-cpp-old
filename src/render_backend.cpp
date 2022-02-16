64bb697c043458bebcbdeac86d14dffaf3a8a4c2
Max Penkov <hello@maxpenkov.com>
2022-02-16 22:37:11 +0200

    Update source file 'render_backend.cpp'

--- src/render_backend.cpp
+++ src/render_backend.cpp
@@ -3,13 +3,19 @@
 #include <cstdint>
 #include "imgui/imgui.h"
 
-auto graphics_handler::create_window(const wchar_t* title_name = L"Default IME") -> HWND
-{
-	return imported::create_overlay(title_name);
-}
+extern HWND render_backend::g_window_handle = nullptr;
+extern ID3D11Device* render_backend::g_d3d11_device = nullptr;
+extern ID3D11RenderTargetView* render_backend::g_d3d11_render_target = nullptr;
+extern ID3D11DeviceContext* render_backend::g_d3d11_device_context = nullptr;
+extern IDXGISwapChain* render_backend::g_swapchain = nullptr;
 
-auto graphics_handler::create_native_window(HWND& window_handle, WNDCLASSEX& window_class,WNDPROC wndproc, const wchar_t* class_name, const wchar_t* window_name, 
-	int posX, int posY, int width, int height, DWORD extended_window_style, DWORD window_style) -> BOOL
+extern int render_backend::g_window_height = 0;
+extern int render_backend::g_window_width = 0;
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+BOOL render_backend::create_window(HWND& window_handle, WNDCLASSEX& window_class,WNDPROC wndproc, const char* class_name, const char* window_name, 
+	int posX, int posY, int width, int height, DWORD extended_window_style, DWORD window_style)
 {
 	ZeroMemory(&window_class, sizeof(window_class));
 
@@ -50,7 +56,7 @@ auto graphics_handler::create_native_window(HWND& window_handle, WNDCLASSEX& win
 	return window_handle != nullptr;
 }
 
-auto graphics_handler::get_window_size(HWND handle, int& height, int& width) -> BOOL
+BOOL render_backend::get_window_size(HWND handle, int& height, int& width)
 {
 	RECT rect;
 
@@ -62,7 +68,7 @@ auto graphics_handler::get_window_size(HWND handle, int& height, int& width) -> 
 	return result;
 }
 
-auto graphics_handler::create_d3d11_device(const HWND window_handle, IDXGISwapChain*& swapchain, ID3D11Device*& device, ID3D11DeviceContext*& device_context) -> BOOL
+BOOL render_backend::d3d11_create_device(const HWND window_handle, IDXGISwapChain*& swapchain, ID3D11Device*& device, ID3D11DeviceContext*& device_context)
 {
 	if(!window_handle)
 	{
@@ -74,13 +80,13 @@ auto graphics_handler::create_d3d11_device(const HWND window_handle, IDXGISwapCh
 	md.Height					= 0;
 	md.Width					= 0;
 	md.Format					= DXGI_FORMAT_B8G8R8A8_UNORM;
-	md.RefreshRate					= DXGI_RATIONAL { 60, 1 };
+	md.RefreshRate				= DXGI_RATIONAL { 60, 1 };
 	
 	DXGI_SWAP_CHAIN_DESC cd { 0 };
 
 	cd.BufferCount					= 1;
 	cd.BufferDesc					= md;
-	cd.Windowed					= TRUE;
+	cd.Windowed						= TRUE;
 	cd.OutputWindow					= window_handle;
 	cd.SampleDesc					= DXGI_SAMPLE_DESC { 1, 0 };
 	cd.BufferUsage					= DXGI_USAGE_RENDER_TARGET_OUTPUT;
@@ -97,18 +103,18 @@ auto graphics_handler::create_d3d11_device(const HWND window_handle, IDXGISwapCh
 
 	auto result = D3D11CreateDeviceAndSwapChain
 	(
-		nullptr,				// DXGI adapter
-		D3D_DRIVER_TYPE_HARDWARE,		// Driver type
-		nullptr,				// Software
+		nullptr,							// DXGI adapter
+		D3D_DRIVER_TYPE_HARDWARE,			// Driver type
+		nullptr,							// Software
 		D3D11_CREATE_DEVICE_BGRA_SUPPORT,	// Flags
-		feature_level_array,			// Feature levels
+		feature_level_array,				// Feature levels
 		_countof(feature_level_array),		// Feature levels count
-		D3D11_SDK_VERSION,			// SDK version
-		&cd,					// Swapchain descriptor
-		&swapchain,				// Swapchain pointer
-		&device,				// D3D11 device pointer
-		nullptr,				// Feature levels supported
-		&device_context				// D3D11 device context pointer
+		D3D11_SDK_VERSION,					// SDK version
+		&cd,								// Swapchain descriptor
+		&swapchain,							// Swapchain pointer
+		&device,							// D3D11 device pointer
+		nullptr,							// Feature levels supported
+		&device_context						// D3D11 device context pointer
 	);
 
 	if(FAILED(result))
@@ -119,7 +125,16 @@ auto graphics_handler::create_d3d11_device(const HWND window_handle, IDXGISwapCh
 	return TRUE;
 }
 
-auto graphics_handler::create_d3d11_render_target(IDXGISwapChain* swapchain, ID3D11Device* device, ID3D11RenderTargetView*& render_target) -> BOOL
+void render_backend::d3d11_release_device(ID3D11Device* device)
+{
+	if (device)
+	{
+		device->Release();
+		device = nullptr;
+	}
+}
+
+BOOL render_backend::d3d11_create_render_target(IDXGISwapChain* swapchain, ID3D11Device* device, ID3D11RenderTargetView*& render_target)
 {
 	ID3D11Texture2D* back_buffer;
 
@@ -137,106 +152,25 @@ auto graphics_handler::create_d3d11_render_target(IDXGISwapChain* swapchain, ID3
 	return result == S_OK ? TRUE : FALSE;
 }
 
-auto graphics_handler::create_d2d1_device_context(IDXGISwapChain* swapchain, ID3D11Device* device, ID2D1DeviceContext*& device_context) -> BOOL
+void render_backend::d3d11_release_render_target(ID3D11RenderTargetView* render_target)
 {
-	// How to render using a Direct2D device context
-	// https://docs.microsoft.com/en-us/windows/win32/direct2d/devices-and-device-contexts#how-to-create-a-direct2d-device-context-for-rendering
-	
-	if(!swapchain)
+	if (render_target)
 	{
-		return FALSE;
+		render_target->Release();
+		render_target = nullptr;
 	}
-
-	IDXGIDevice* dxgi_device;
-	if(device->QueryInterface(__uuidof(IDXGIDevice), reinterpret_cast<void**>(&dxgi_device)) != S_OK)
-	{
-		return FALSE;
-	}
-	
-	ID2D1Factory1* d2d1_factory;
-	if(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &d2d1_factory) != S_OK)
-	{
-		return FALSE;
-	}
-
-	ID2D1Device* d2d1_device;
-	if(d2d1_factory->CreateDevice(dxgi_device, &d2d1_device) != S_OK)
-	{
-		return FALSE;
-	}
-	
-	if(d2d1_device->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &device_context) != S_OK)
-	{
-		return FALSE;
-	}
-
-	float dpiX, dpiY;
-	d2d1_factory->GetDesktopDpi(&dpiX, &dpiY);
-	
-	D2D1_BITMAP_PROPERTIES1 bitmap_prop = D2D1::BitmapProperties1
-	(
-		D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
-		D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE),
-		dpiX,
-		dpiY
-	);
-
-	IDXGISurface* dxgi_surface;
-	if(swapchain->GetBuffer(0, IID_PPV_ARGS(&dxgi_surface)) != S_OK)
-	{
-		return FALSE;
-	}
-
-	ID2D1Bitmap1* target_bitmap;
-	if(device_context->CreateBitmapFromDxgiSurface(dxgi_surface, &bitmap_prop, &target_bitmap) != S_OK)
-	{
-		return FALSE;
-	}
-
-	device_context->SetTarget(target_bitmap);
-
-	return TRUE;
 }
 
-auto graphics_handler::create_d2d11_render_target(IDXGISwapChain* swapchain, ID2D1RenderTarget*& d2d1_render_target) -> BOOL
+void render_backend::d3d11_release_device_context(ID3D11DeviceContext* device_context)
 {
-	if(!swapchain)
+	if (device_context)
 	{
-		return FALSE;
+		device_context->Release();
+		device_context = nullptr;
 	}
-
-	IDXGISurface* dxgi_surface;
-	if(swapchain->GetBuffer(0, IID_PPV_ARGS(&dxgi_surface)) != S_OK)
-	{
-		return FALSE;
-	}
-
-	ID2D1Factory1* d2d1_factory;
-	if(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &d2d1_factory) != S_OK)
-	{
-		return FALSE;
-	}
-
-	float dpiX, dpiY;
-	d2d1_factory->GetDesktopDpi(&dpiX, &dpiY);
-	
-	D2D1_RENDER_TARGET_PROPERTIES prop = D2D1::RenderTargetProperties
-	(
-		D2D1_RENDER_TARGET_TYPE_HARDWARE,
-		D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED),
-		dpiX,
-		dpiY
-	);
-
-	if(d2d1_factory->CreateDxgiSurfaceRenderTarget(dxgi_surface, &prop, &d2d1_render_target) != S_OK)
-	{
-		return FALSE;
-	}
-	
-	return TRUE;
 }
 
-auto graphics_handler::release_dxgi_swapchain(IDXGISwapChain* swapchain) -> void
+void render_backend::dxgi_release_swapchain(IDXGISwapChain* swapchain)
 {
 	if(swapchain)
 	{
@@ -245,47 +179,86 @@ auto graphics_handler::release_dxgi_swapchain(IDXGISwapChain* swapchain) -> void
 	}
 }
 
-auto graphics_handler::release_d3d11_device(ID3D11Device* device) -> void
+LRESULT render_backend::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	 if(device)
-	 {
-	 	device->Release();
-	 	device = nullptr;
-	 }
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+	{
+		return true;
+	}
+
+	switch (msg)
+	{
+	case WM_SIZE:
+
+		if (g_d3d11_device != nullptr && wParam != SIZE_MINIMIZED)
+		{
+			d3d11_release_render_target(g_d3d11_render_target);
+
+			g_swapchain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_B8G8R8A8_UNORM, 0);
+
+			render_backend::d3d11_create_render_target(g_swapchain, g_d3d11_device, g_d3d11_render_target);
+		}
+
+		return true;
+
+	case WM_SYSCOMMAND:
+
+		if ((wParam & 0xFFF0) == SC_KEYMENU)
+		{
+			return 0;
+		}
+
+		break;
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-auto graphics_handler::release_d3d11_device_context(ID3D11DeviceContext* device_context) -> void
-{
-	if(device_context)
-	{
-		device_context->Release();
-		device_context = nullptr;
+BOOL render_backend::initialize() {
+
+	static WNDCLASSEX wndclassex;
+
+	create_window(
+		g_window_handle,
+		wndclassex,
+		WndProc,
+		"Chrome_WidgetWin_1",
+		"Core Input",
+		0,
+		0,
+		1920,
+		1080,
+		WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_NOACTIVATE | WS_EX_TOPMOST,
+		WS_VISIBLE | WS_MAXIMIZE);
+
+	if (d3d11_create_device(g_window_handle, g_swapchain, g_d3d11_device, g_d3d11_device_context) == FALSE) {
+		terminate();
+		return FALSE;
 	}
+
+	if (d3d11_create_render_target(g_swapchain, g_d3d11_device, g_d3d11_render_target) == FALSE) {
+		terminate();
+		return FALSE;
+	}
+
+	SetWindowLongPtrA(g_window_handle, GWLP_WNDPROC, reinterpret_cast<int64_t>(WndProc));
+
+	ShowWindow(g_window_handle, SW_SHOW);
+	UpdateWindow(g_window_handle);
+
+	get_window_size(g_window_handle, g_window_height, g_window_width);
+
+	MoveWindow(g_window_handle, 0, 0, g_window_width, g_window_height, FALSE);
 }
 
-auto graphics_handler::release_d3d11_render_target(ID3D11RenderTargetView* render_target) -> void
-{
-	if(render_target)
-	{
-		render_target->Release();
-		render_target = nullptr;
-	}
-}
-
-auto graphics_handler::release_d2d1_device_context(ID2D1DeviceContext* device_context) -> void
-{
-	if(device_context)
-	{
-		device_context->Release();
-		device_context = nullptr;
-	}
-}
-
-auto graphics_handler::release_d2d11_render_target(ID2D1RenderTarget* d2d1_render_target) -> void
-{
-	if(d2d1_render_target)
-	{
-		d2d1_render_target->Release();
-		d2d1_render_target = nullptr;
-	}
+void render_backend::terminate() {
+	d3d11_release_render_target(g_d3d11_render_target);
+	d3d11_release_device(g_d3d11_device);
+	dxgi_release_swapchain(g_swapchain);
+	DestroyWindow(g_window_handle);
 }
