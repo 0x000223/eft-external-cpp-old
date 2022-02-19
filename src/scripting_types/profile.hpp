aee9ad9338b419ba9d0859cd9fef8996aa9a5af9
Max Penkov <hello@maxpenkov.com>
2022-02-19 19:02:06 +0200

    Update 'src/scripting_types/profile.hpp' header file

--- src/scripting_types/profile.hpp
+++ src/scripting_types/profile.hpp
@@ -1,16 +1,16 @@
-/**
- * @file profile.hpp
- * @date 12/02/2022
- */
+/*****************************************************************//**
+ * \file   profile.hpp
+ * \brief  
+ * 
+ * \author Max
+ * \date   February 2022
+ *********************************************************************/
 
 #ifndef PROFILE_HPP
 #define PROFILE_HPP
 
-#include <string>
-
 #include "offset.hpp"
 #include "memory.hpp"
-#include "e_player_side.h"
 
 /**
  * @brief Class which represents EFT.Profile
@@ -24,19 +24,16 @@ private:
 	 */
 	address_t m_address;
 
-	std::wstring id;
+	/**
+	 * @brief Address of Info class
+	 */
+	address_t m_info_address;
 
-	std::wstring account_id;
+	//e_player_side side;
 
-	std::wstring nickname;
+	//std::string side_name;
 
-	std::wstring group_id;
-
-	e_player_side side;
-
-	std::string side_name;
-
-	auto parse_side_name() const -> std::string
+	/*auto parse_side_name() const -> std::string
 	{
 		switch(side)
 		{
@@ -52,68 +49,39 @@ private:
 		default:
 			return std::string("");
 		}
-	}
+	}*/
 
 public:
 
+	/**
+	 * @brief
+	 */
+	std::wstring m_id;
+
+	/**
+	 * @brief Represents AccountId field in EFT.Profile class
+	 */
+	std::wstring m_account_id;
+
+	/**
+	 * @brief Represents GroupId in Info class
+	 */
+	std::wstring m_group_id;
+
+	/**
+	 * @brief Represents Nickname field in Info class
+	 */
+	std::wstring m_nickname;
+
 	profile() 
-		: m_address(0)
+		: m_address(0), m_info_address(0)
 	{}
 
 	explicit profile(const address_t address) 
-		: m_address(address)
-	{
-		id = 
-			memory_handler::read_wide_string(addr + offset::player::profile::id);
-
-		account_id = 
-			memory_handler::read_wide_string(addr + offset::player::profile::account_id);
-
-		auto info =
-			memory_handler::read<uintptr_t>(addr + offset::player::profile::profile_info);
-
-		nickname =
-			memory_handler::read_wide_string(info + offset::profile::info::nickname);
-
-		group_id =
-			memory_handler::read_wide_string(info + offset::profile::info::group_id);
-
-		side = 
-			static_cast<e_player_side>(
-				memory_handler::read<int>(info + offset::profile::info::side));
-
-		side_name = parse_side_name();
-	}
-
-	auto get_id() const -> std::wstring
-	{
-		return id;
-	}
-
-	auto get_account_id() const -> std::wstring
-	{
-		return account_id;
-	}
-
-	auto get_nickname() const -> std::wstring
-	{
-		return nickname;
-	}
-
-	auto get_group_id() const -> std::wstring
-	{
-		return group_id;
-	}
-
-	auto get_side() const -> e_player_side
-	{
-		return side;
-	}
-
-	auto get_side_name() const -> std::string
-	{
-		return side_name;
-	}
+		: m_address(address),
+		m_info_address(memory::read<address_t>(address + O_PROFILE_INFO)),
+		m_nickname(memory::read_wide_string(m_info_address + O_PROFILE_INFO_NICKNAME))
+	{}
 };
 
 #endif
