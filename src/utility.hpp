33632e8ba1a522057b1816dd8e194bd68a8d01d7
Max Penkov <hello@maxpenkov.com>
2022-02-19 18:56:47 +0200

    Update header file 'src/utility.hpp' - add 'parse_list'

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,29 +1,50 @@
+/*****************************************************************//**
+ * \file   utility.hpp
+ * \brief  
+ * 
+ * \author Max
+ * \date   February 2022
+ *********************************************************************/
 
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
 
 #include <string>
+#include <vector>
 #include <locale>
 #include <codecvt>
 #include <cmath>
 
-#pragma warning (disable : 4996)
+#include "offset.hpp"
+#include "memory.hpp"
 
 namespace utility
 {
-	inline auto wide_to_narrow(const std::wstring& wide_string)
-	{
-		if(wide_string.size() > 1000)
-		{
-			return std::string();
+	/**
+	 * @brief Parses System.Collections.Generic.List<T>
+	 */
+	template<typename T>
+	std::vector<T> parse_list(const address_t address) {
+		const auto list_address = memory::read<address_t>(address);
+		
+		if (address == 0) {
+			return std::vector<T>();
 		}
-		
-		using convert_type = std::codecvt_utf8<wchar_t>;
-		
-		std::wstring_convert<convert_type, wchar_t> converter;
 
-		return std::string { converter.to_bytes(wide_string) };
+		std::vector<T> ret;
+
+		const auto count = memory::read<int>(list_address + O_LIST_SIZE);
+		const auto base = memory::read<address_t>(list_address + O_LIST_ITEMS);
+
+		for (auto index = 0; index < count; ++index) {
+			const auto addr = memory::read<address_t>(base + O_ARRAY_BASE + index * 0x8);
+			ret.push_back(T(addr));
+		}
+
+		return ret;
 	}
+
+	std::string wide_to_narrow(const std::wstring& wide_string);
 }
 
 #endif
