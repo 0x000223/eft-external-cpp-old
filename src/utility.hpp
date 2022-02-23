01a4b70387bef4157cc512e8c4143295015fe566
Max Penkov <hello@maxpenkov.com>
2022-02-23 23:03:37 +0200

    Update header file 'utility.hpp'

--- src/utility.hpp
+++ src/utility.hpp
@@ -14,24 +14,32 @@
 #include <locale>
 #include <codecvt>
 #include <cmath>
+#include <concepts>
 
 #include "offset.hpp"
 #include "memory.hpp"
 
 namespace utility
 {
+	template<typename T>
+	concept Integral = std::is_integral<T>::value;
+	
+	template<typename T>
+	concept NonIntegral = !std::is_integral<T>::value;
+
 	/**
 	 * @brief Parses System.Collections.Generic.List<T>
 	 */
-	template<typename T>
+	template<NonIntegral T>
 	std::vector<T> parse_list(const address_t address) {
+
 		const auto list_address = memory::read<address_t>(address);
 		
-		if (address == 0) {
-			return std::vector<T>();
-		}
-
 		std::vector<T> ret;
+
+		if (address == 0) {
+			return ret;
+		}
 
 		const auto count = memory::read<int>(list_address + O_LIST_SIZE);
 		const auto base = memory::read<address_t>(list_address + O_LIST_ITEMS);
@@ -39,6 +47,28 @@ namespace utility
 		for (auto index = 0; index < count; ++index) {
 			const auto addr = memory::read<address_t>(base + O_ARRAY_BASE + index * 0x8);
 			ret.push_back(T(addr));
+		}
+
+		return ret;
+	}
+
+	template<Integral T>
+	std::vector<T> parse_list(const address_t address) {
+
+		const auto list_address = memory::read<address_t>(address);
+
+		std::vector<T> ret;
+
+		if (address == 0) {
+			return ret;
+		}
+
+		const auto count = memory::read<int>(list_address + O_LIST_SIZE);
+		const auto base = memory::read<address_t>(list_address + O_LIST_ITEMS);
+
+		for (auto index = 0; index < count; ++index) {
+			const auto value = memory::read<address_t>(base + O_ARRAY_BASE + index * 0x8);
+			ret.push_back(value);
 		}
 
 		return ret;
