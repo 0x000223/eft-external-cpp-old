2a43a9613cf15dd0525104774e419dfdbeba4f47
Max Penkov <hello@maxpenkov.com>
2022-02-26 20:27:50 +0200

    Add header file 'feature.hpp'

---
+++ src/feature.hpp
@@ -0,0 +1,22 @@
+
+#ifndef FEATURE_HPP
+#define FEATURE_HPP
+
+#include "world.hpp"
+
+static player get_best_target();
+
+namespace feature {
+
+	/**
+	 * @brief
+	 */
+	void aimbot_fov();
+
+	/**
+	 * @brief
+	 */
+	void no_recoil();
+}
+
+#endif
