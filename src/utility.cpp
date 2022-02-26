c3862fadb1dc0910773448821c360982d94e6d26
Max Penkov <hello@maxpenkov.com>
2022-02-26 20:40:17 +0200

    Update source file 'utility.cpp'

--- src/utility.cpp
+++ src/utility.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 string utility::wide_to_narrow(const wstring& wide_string) {
-	if (wide_string.size() > 1000)
-	{
-		return string();
+
+	string ret;
+
+	if (wide_string.size() > 1000) {
+		return ret;
 	}
 
 	using convert_type = codecvt_utf8<wchar_t>;
 
 	static wstring_convert<convert_type, wchar_t> converter;
 
-	return string{ converter.to_bytes(wide_string) };
+	return ret.assign( converter.to_bytes(wide_string) );
 }
