fc1227a13c6f64e7290f32a9772eebefe2a4bac3
Max Penkov <hello@maxpenkov.com>
2022-02-26 20:29:20 +0200

    Update header file 'transform.hpp'

--- src/unity_types/transform.hpp
+++ src/unity_types/transform.hpp
@@ -12,12 +12,45 @@
 #include "math.hpp"
 #include "component.hpp"
 
+struct trsX { // sizeof=0x30
+	// math::_float3 
+	// math::_float4
+	// math::_float3
+};
+
 /**
  * @brief Class which represents UnityEngine::Transform
  */
 class transform : public component
 {
-	
+
+private:
+
+	/**
+	 * @brief Index of current transform
+	 */
+	int index;
+
+	/**
+	 * @brief Address of TransformHierarchy structure
+	 */
+	address_t hierarchy_address;
+
+	/**
+	 * @brief
+	 */
+	address_t local_transforms_address;
+
+	/**
+	 * @brief
+	 */
+	address_t parent_indices_address;
+
+	/**
+	 * @brief
+	 */
+	std::vector<int> parent_indices;
+
 public:
 
 	transform() 
@@ -25,7 +58,12 @@ public:
 	{}
 	
 	explicit transform(const address_t address)
-		: component(address) 
+		: component(address),
+		hierarchy_address(memory::read<address_t>(address + O_TRANSFORM_ACCESS + O_TRANSFROM_ACCESS_HIERARCHY)),
+		index(memory::read<int>(address + O_TRANSFORM_ACCESS + O_TRANSFORM_ACCESS_INDEX)),
+		local_transforms_address(memory::read<address_t>(hierarchy_address + O_TRANSFORM_ACCESS_HIERARCHY_LOCALTRANSFORMS)),
+		parent_indices_address(memory::read<address_t>(hierarchy_address + O_TRANSFORM_ACCESS_HIERARCHY_PARENTINDICES)),
+		parent_indices(memory::read_many<int>(parent_indices_address, index + 1))
 	{}
 	
 	/**
